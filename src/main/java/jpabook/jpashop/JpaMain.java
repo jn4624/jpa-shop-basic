608f08320893cebe2335292bc01582db13468f9a
LimJiNa <jn4624@gmail.com>
2024-05-16 15:16:21 +0900

    상속 관계 매핑 설정

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Book;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,7 +15,11 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
 
+            em.persist(book);
 
             tx.commit();
         } catch (Exception e) {
