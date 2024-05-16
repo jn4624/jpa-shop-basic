0d27c7c5adeba6d480c78cde89a0b9bb602f5829
LimJiNa <jn4624@gmail.com>
2024-05-16 22:44:07 +0900

    임베디드 타입 매핑 설정

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -15,12 +15,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Book book = new Book();
-            book.setName("JPA");
-            book.setAuthor("김영한");
-
-            em.persist(book);
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
