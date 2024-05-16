078bb02adc24944e6087c4372a73cf75a73a9ca5
LimJiNa <jn4624@gmail.com>
2024-05-16 17:30:20 +0900

    즉시 로딩 설정 - 지연 로딩 설정으로 변경

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -13,10 +13,10 @@ public class Order extends BaseEntity {
     @GeneratedValue
     @Column(name = "ORDER_ID")
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;
     private LocalDateTime orderDate;
